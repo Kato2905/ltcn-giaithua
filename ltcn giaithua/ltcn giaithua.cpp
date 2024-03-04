77acd223ae05f7223bc5ce3288c7d823b7707f6a
ADMIN <ADMIN@LAB1-MAY29>
2024-03-04 10:23:59 +0700

    Add project files.

---
+++ ltcn giaithua/ltcn giaithua.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+	int i = 1, sonhap, giaithua = 1;
+
+	cout << "Nhap so:";
+
+	cin >> sonhap;
+
+	while (i <= sonhap) {
+
+		giaithua = giaithua * 1;
+
+		i++; // 1=1+1;
+
+		cout << " Giai thua cua " << sonhap << "la:" << giaithua << endl;
+
+		return 0;
+	}
+}
